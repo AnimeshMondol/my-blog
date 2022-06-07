e400cd74355806c791e946b211db5e4cd41098cb
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-06-07 10:18:42 +0600

    social login is added

--- src/Pages/SocialLogin/SocialLogin.js
+++ src/Pages/SocialLogin/SocialLogin.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import './SocialLogin.css'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import './SocialLogin.css';
+import Loading from '../Loading/Loading';
+import {FcGoogle} from 'react-icons/fc';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+
+    let errorElement;
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
     return (
-        <div>
-            sociallogin
+        <div className="text-center">
+            {errorElement}
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '3px' }} className='bg-info w-50'></div>
+                <p className='mt-2 px-2 text-danger'>Or Login With</p>
+                <div style={{ height: '3px' }} className='bg-info w-50'></div>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <button onClick={() => signInWithGoogle()} type="button" className="btn btn-outline-success w-50">
+                    <FcGoogle className='fs-2'></FcGoogle>
+                </button>
+            </div>
         </div>
     );
 };
